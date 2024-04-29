24192b1aa9de383ceda9fe433361b6dd3c05264f
Nahyeon Kang <kathy95917@naver.com>
2024-04-30 00:02:00 +0900

    feat: food 객체 데이터 추가

--- 01_FoodMenuApp/js/data.js
+++ 01_FoodMenuApp/js/data.js
@@ -1,1 +1,54 @@
-const data = {};
+import LOREMIPSUM from './const.js';
+
+const data = [
+  {
+    Breakfast: {
+      pancakes: {
+        price: 3000,
+        description: LOREMIPSUM,
+      },
+    },
+  },
+  {
+    Lunch: {
+      diner: {
+        price: 1000,
+        description: LOREMIPSUM,
+      },
+      'egg attack': {
+        price: 2000,
+        description: LOREMIPSUM,
+      },
+      'american classic': {
+        price: 4000,
+        description: LOREMIPSUM,
+      },
+    },
+  },
+  {
+    Shakes: {
+      milkshake: {
+        price: 2000,
+        description: LOREMIPSUM,
+      },
+      'oreo dream': {
+        price: 7000,
+        description: LOREMIPSUM,
+      },
+      'quarantine buddy': {
+        price: 1000,
+        description: LOREMIPSUM,
+      },
+    },
+  },
+  {
+    Dinner: {
+      'bison steak': {
+        price: 2000,
+        description: LOREMIPSUM,
+      },
+    },
+  },
+];
+
+export default data;
