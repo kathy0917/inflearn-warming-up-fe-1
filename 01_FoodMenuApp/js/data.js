7214412fdece3a8fb3633fb1f3f40a6fd00acb7e
Nahyeon Kang <kathy95917@naver.com>
2024-04-30 00:34:09 +0900

    fix: 음식 데이터 구조 수정

--- 01_FoodMenuApp/js/data.js
+++ 01_FoodMenuApp/js/data.js
@@ -2,52 +2,76 @@ import LOREMIPSUM from './const.js';
 
 const data = [
   {
-    Breakfast: {
-      pancakes: {
-        price: 3000,
-        description: LOREMIPSUM,
+    id: 1,
+    title: 'Breakfast',
+    type: [
+      {
+        pancakes: {
+          price: 3000,
+          description: LOREMIPSUM,
+        },
       },
-    },
+    ],
   },
   {
-    Lunch: {
-      diner: {
-        price: 1000,
-        description: LOREMIPSUM,
+    id: 2,
+    title: 'Lunch',
+    type: [
+      {
+        diner: {
+          price: 1000,
+          description: LOREMIPSUM,
+        },
       },
-      'egg attack': {
-        price: 2000,
-        description: LOREMIPSUM,
+      {
+        'egg attack': {
+          price: 2000,
+          description: LOREMIPSUM,
+        },
       },
-      'american classic': {
-        price: 4000,
-        description: LOREMIPSUM,
+      {
+        'american classic': {
+          price: 4000,
+          description: LOREMIPSUM,
+        },
       },
-    },
+    ],
   },
   {
-    Shakes: {
-      milkshake: {
-        price: 2000,
-        description: LOREMIPSUM,
+    id: 3,
+    title: 'Shakes',
+    type: [
+      {
+        milkshake: {
+          price: 2000,
+          description: LOREMIPSUM,
+        },
       },
-      'oreo dream': {
-        price: 7000,
-        description: LOREMIPSUM,
+      {
+        'oreo dream': {
+          price: 7000,
+          description: LOREMIPSUM,
+        },
       },
-      'quarantine buddy': {
-        price: 1000,
-        description: LOREMIPSUM,
+      {
+        'quarantine buddy': {
+          price: 1000,
+          description: LOREMIPSUM,
+        },
       },
-    },
+    ],
   },
   {
-    Dinner: {
-      'bison steak': {
-        price: 2000,
-        description: LOREMIPSUM,
+    id: 4,
+    title: 'Dinne',
+    type: [
+      {
+        'bison steak': {
+          price: 2000,
+          description: LOREMIPSUM,
+        },
       },
-    },
+    ],
   },
 ];
 
